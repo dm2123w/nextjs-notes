0cb934ce44c71fba92d8c6d33e16460879ea3d68
dmitriysemenko <dm123semenko@gmail.com>
2021-10-24 01:51:33 +0500

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Link from "next/link";
 
-const Page = () => (
-    <div>
-        <h1>Index Page</h1>
-        <Link href="/notes">
-            <a>Link</a>
-        </Link>
+export default () => (
+    <div sx={{ height: `calc(100vh - 60px)` }}>
+        <div
+            sx={{
+                variant: "containers.page",
+                display: "flex",
+                alignItems: "center",
+                height: "100%",
+            }}
+        >
+            <h1 sx={{ fontSize: 8, my: 0 }}>Note App</h1>
+        </div>
     </div>
 );
-
-export default Page;
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,12 +1,15 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-const Page = () => {
+export default () => {
     const router = useRouter();
-
     const { id } = router.query;
 
-    return <h1>Note {id}</h1>;
+    return (
+        <div sx={{ variant: "containers.page" }}>
+            <h1>Note: {id} </h1>
+        </div>
+    );
 };
-
-export default Page;
